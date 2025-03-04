4e3393b3d859b9f262d01c95b91bb3fbaa617b8c
Oliver <sangoliver@gmail.com>
2025-03-04 21:58:50 +0300

    BoardGame

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,26 @@
 import React from "react";
+import Tile from "./Tile";
 
-import Tile from "Tile";
+function GameBoard(props) {
+  const { board, onTileClick } = props;
+
+  return (
+    <div className="board">
+      {board.map(function (row, rowIndex) {
+        return row.map(function (letter, colIndex) {
+          return (
+            <Tile
+              key={`${rowIndex}-${colIndex}`}
+              letter={letter}
+              onClick={function () {
+                onTileClick(rowIndex, colIndex);
+              }}
+            />
+          );
+        });
+      })}
+    </div>
+  );
+}
+
+export default GameBoard;
